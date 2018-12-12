ca67ac5677129ea0a7ce0eb93c4423d3cb10e166
proelbtn <proelbtn@users.noreply.github.com>
2018-12-13 03:03:51 +0900

    write sample ping-pong code

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
-function hello(compiler: string) {
-    console.log(`Hello from ${compiler}`);
-}
-hello("TypeScript");
+import * as Discord from "discord.js";
+import * as dotenv from "dotenv";
+
+// load .env
+dotenv.config();
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.channel.send("pong");
+  }
+});
+
+client.login(process.env.ILJJMKIX_TOKEN);
